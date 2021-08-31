a1c9fa4ab38f797cb03256655714d51354729bb4
jonas <jonas.umaras@gmail.com>
2021-08-31 21:11:32 +0300

    String concat vs. string builder.

---
+++ BenchmarkerApp/Demo.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+namespace BenchmarkerApp
+{
+    [MemoryDiagnoser]
+    public class Demo
+    {
+        [Benchmark]
+        public string GetFullStringNormally()
+        {
+            string output = "";
+
+            for (int i = 0; i < 100; i++) output += i; 
+
+            return output;
+        }
+
+        [Benchmark]
+        public string GetFullStringBuilder()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < 100; i++) sb.Append(i);
+
+            return sb.ToString();
+        }
+    }
+}
